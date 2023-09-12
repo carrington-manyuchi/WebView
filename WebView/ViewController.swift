8abb3117ba186e774d82e2d6225e199b0e6189b6
Carrington <Manyuchi>
2023-09-12 09:22:31 +0200

    Initial Commit

---
+++ WebView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebView
+//
+//  Created by DA MAC M1 157 on 2023/09/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
